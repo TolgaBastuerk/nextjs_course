06bd832ad04358bdebfdd55e538857f2f00467e5
Tolga Bastuerk <tolga.bastuerk@outlook.com>
2022-03-27 20:47:28 +0200

    minor change

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,5 +1,6 @@
-import MainNavigation from './MainNavigation';
-import classes from './Layout.module.css';
+import MainNavigation from "./MainNavigation";
+import classes from "./Layout.module.css";
+import Link from "next/Link";
 
 function Layout(props) {
   return (
